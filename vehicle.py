cd22b415f31881bfa8e55d231b8f1e12211f2e6a
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-26 14:28:32 -0600

    Created vehicle class

---
+++ vehicle.py
@@ -0,0 +1,5 @@
+class Vehicle():
+    def __init__(self, main_color, max_occupancy, powered_by):
+        self.main_color = main_color
+        self.max_occupancy = max_occupancy
+        self.powered_by = powered_by
