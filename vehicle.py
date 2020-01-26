c4c3264cc792aae05eea47da9dfb82103ce5211d
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-26 14:37:38 -0600

    Created Baby class

--- vehicle.py
+++ vehicle.py
@@ -1,5 +1,7 @@
 class Vehicle():
-    def __init__(self, main_color, max_occupancy, powered_by):
+    def __init__(self, main_color, max_occupancy, powered_by, make, model):
         self.main_color = main_color
         self.max_occupancy = max_occupancy
         self.powered_by = powered_by
+        self.make = make
+        self.model = model
