1a454bdde155d61426ab87bb20a56f376a4e7cf0
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-26 14:38:56 -0600

    Created Batmobile class

---
+++ batmobile.py
@@ -0,0 +1,6 @@
+from vehicle import Vehicle
+
+class Batmobile(Vehicle):
+    def __init__(self, main_color, max_occupancy, powered_by, make, model, special_feature):
+        super().__init__(main_color, max_occupancy, powered_by, make, model)
+        self.special_feature = special_feature
